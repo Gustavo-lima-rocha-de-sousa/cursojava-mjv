ab9acc0ccffc419df1d6fd2b58e604d56fe741cb
Gustavo Lima <gustavolrs18@gmail.com>
2021-12-17 10:11:21 -0300

    Aula-16-12-21

---
+++ src/aula/TerminalBancario.java
@@ -0,0 +1,16 @@
+package aula;
+
+public class TerminalBancario {
+
+	public static void main(String[] args) {
+
+		System.out.println(somar(2, 3));
+
+	}
+
+	private static int somar(int n1, int n2) {
+		int resultado = n1 + n2;
+		return resultado = n1 + n2;
+	}
+
+}
