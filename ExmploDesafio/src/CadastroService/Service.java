18906410f17403df225e94e715695ab154df4265
Gustavo Lima <gustavolrs18@gmail.com>
2021-12-21 18:27:41 -0300

    Exemplo Desafio.

--- ExmploDesafio/src/CadastroService/Service.java
+++ ExmploDesafio/src/CadastroService/Service.java
@@ -1,12 +1,17 @@
 package CadastroService;
 
+import java.util.Date;
+
 import pessoas.Pessoais;
 
 public class Service {
 
-	public void cadastrar (Pessoais pessoais, String nome ) {
-			pessoais.setNome(nome);		
-				
+	public void cadastrar (Pessoais pessoais, String nome, String cpf, String email, Date aniversario, String foto, Integer id) {
+			pessoais.setNome(pessoais.getNome() + nome);
+			pessoais.setCpf(pessoais.getCpf() + cpf);
+			pessoais.setEmail(pessoais.getEmail() + email);
+			pessoais.setAniversario(pessoais.getAniversario() + aniversario);
+			pessoais.setFoto(pessoais.getFoto() + foto);
 	}
-	
+		
 }
--- ExmploDesafio/src/cadastro/CadastroApplication.java
+++ ExmploDesafio/src/cadastro/CadastroApplication.java
@@ -1,15 +1,11 @@
 package cadastro;
 
+import CadastroService.Service;
+import pessoas.Pessoais;
 
 public class CadastroApplication {
 
 	public static void main(String[] args) {
 		
-		
-		
-		
-		
 	}
-	
-	
 }
